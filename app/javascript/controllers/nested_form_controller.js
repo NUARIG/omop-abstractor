b803e875fe30b2fed792931197b1f0750eab1373
Michael Gurley <m-gurley@northwestern.edu>
2019-12-21 17:55:58 -0500

    references #10 Further refactoring for removal of Cocoon gem.

---
+++ app/javascript/controllers/nested_form_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets =['list', 'links', 'template']
+
+  connect() {
+  }
+
+  add_association(event) {
+    event.preventDefault();
+    var content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
+    // this.linksTarget.insertAdjacentHTML('beforebegin', content);
+    this.listTarget.insertAdjacentHTML('beforeend', content);
+  }
+
+  remove_association(event) {
+    event.preventDefault();
+    let wrapper = event.target.closest('.nested-fields')
+    if (wrapper.dataset.newRecord == "true"){
+      wrapper.remove();
+    }
+    else {
+      wrapper.querySelector("input[name*='_destroy']").value = 1;
+      wrapper.style.display = 'none';
+    }
+  }
+}
